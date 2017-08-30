be15e3d9ac06c7c7a4bdc3d3855ff06c1c5ad5aa
Oscar Muñoz Bernales <munoz.bernales.oscar@gmail.com>
2017-08-29 23:11:33 -0300

    UPDATE: los tipos de eventos en js
    - descripcion de os tipos de eventos que existen en js
    - falta implementarlos dentro del html

---
+++ Proyect-JavaScript/public_html/Js/eventos.js
@@ -0,0 +1,54 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*EVENTOS
+ * EVENTO           | DESCRIPCION                                   | ELEMENTOS PARA LOS QUE ESTA DEFINIDO
+ * ----------------------------------------------------------------------------------------------------------------------------
+ * onblur           | Deseleccionar el elemento                     | <button>, <input>, <label>, <select>, <textarea>, <body>|
+ * ----------------------------------------------------------------------------------------------------------------------------
+ * onchange         | Deseleccionar un elemento que se ha modificado| <input>, <select>, <textarea>                           |
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * onclick          | Pinchar y soltar el ratón                     | Todos los elementos generalmente en los <button>
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * ondblclick       | Pinchar dos veces seguidas con el ratón       | Todos los elementos
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * onfocus          | Seleccionar un elemento                       | <button>, <input>, <label>, <select>, <textarea>, <body>
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * onkeydown        | Pulsar una tecla (sin soltar)                 | Elementos de formulario y <body>
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * onkeypress       | Pulsar una tecla                              | Elementos de formulario y <body>
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * onkeyup          | Soltar una tecla pulsada                      | Elementos de formulario y <body>
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * onload           | La página se ha cargado completamente         | <body>
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * onmousedown      | Pulsar (sin soltar) un botón del ratón        | Todos los elementos
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * onmousemove      | Mover el ratón                                | Todos los elementos
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * onmouseout       | El ratón "sale" del elemento                  | Todos los elementos
+ *                  | (pasa por encima de otro elemento)            |
+ *---------------------------------------------------------------------------------------------------------------------------
+ *onmouseover       | El ratón "entra" en el elemento               |
+ *                  | (pasa por encima del elemento)                | Todos los elementos
+ *---------------------------------------------------------------------------------------------------------------------------
+ *onmouseup         | Soltar el botón que estaba pulsado en el ratón| Todos los elementos
+ *---------------------------------------------------------------------------------------------------------------------------
+ *onreset           | Inicializar el formulario                     |
+ *                  | (borrar todos sus datos)                      | <form>
+ *---------------------------------------------------------------------------------------------------------------------------
+ *onresize          | Se ha modificado el tamaño de la              |
+ *                  | ventana del navegador                         | <body>
+ *---------------------------------------------------------------------------------------------------------------------------
+ *onselect          | Seleccionar un texto                          | <input>, <textarea>
+ *---------------------------------------------------------------------------------------------------------------------------
+ *onsubmit          | Enviar el formulario                          | <form>
+ *---------------------------------------------------------------------------------------------------------------------------
+ *onunload          | Se abandona la página                         |
+ *                  | (por ejemplo al cerrar el navegador)          | <body>
+ *---------------------------------------------------------------------------------------------------------------------------
+ **/
+
+
---
+++ Proyect-JavaScript/public_html/Js/typeof.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+console.log("123 " + typeof 123); //NUMBER
+console.log("{} " + typeof {}); // OBJECT
+console.log("'hola que tal' " + typeof 'hola que tal'); //STRING
+console.log("null " + typeof null); //OBJECT
+console.log("[]" + typeof []); //OBJECT
+console.log("123.123 " + typeof 123.123); //NUMBER
+console.log("'c' " + typeof 'c'); //STRING
+console.log("var " + typeof bar); //UNDEFINE
+
+
+
